9e68789d94334542eaa2714a52626f2f7e7f4234
Ricardo Lemser <ricardo.lemser@gmail.com>
2024-09-21 17:30:24 +0200

    Update UseState

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,25 @@ import Nav from "./components/Nav.tsx";
 import Footer from "./components/Footer.tsx";
 import Article from "./components/Article.tsx";
 import Article2 from "./components/Article2.tsx";
+import {useState} from "react";
+import styled from "styled-components";
+
+
 
 function App() {
+    const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
 
 
     return (
         <>
             <GlobalStyles/>
             <Header/>
+            <StyledDiv>
+                {isLoggedIn ? <p>Willkommen zurück!</p> : <p>Bitte einloggen.</p>}
+                <StyledButton onClick={() => setIsLoggedIn(!isLoggedIn)}>
+                    {isLoggedIn ? 'Ausloggen' : 'Einloggen'}
+                </StyledButton>
+            </StyledDiv>
             <Nav/>
             <main>
                 <Article/>
@@ -24,3 +35,23 @@ function App() {
 }
 
 export default App
+
+
+const StyledButton = styled.div`
+    color: black;
+    align-items: center;
+    text-align: center;
+    font-size: 1em;
+    margin: 50px;
+    width: 10%;
+    padding: 10px;
+    border: 2px solid #303030;
+    border-radius: 3px;
+`;
+
+const StyledDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+`
