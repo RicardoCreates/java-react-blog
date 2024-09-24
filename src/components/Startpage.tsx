fbf981c246691cd537a333e5f5912593c8a47664
Ricardo Lemser <ricardo.lemser@gmail.com>
2024-09-24 12:38:38 +0200

    final mobile

--- src/components/Startpage.tsx
+++ src/components/Startpage.tsx
@@ -1,10 +1,17 @@
 import {Link} from "react-router-dom";
+import styled from "styled-components";
 
 export default function Startpage() {
     return (
         <>
+            <StyledDiv>
             <h1>Welcome to my blog</h1>
             <Link to={'/article'}>Article</Link>
+            </StyledDiv>
         </>
     )
 }
+
+const StyledDiv = styled.div`
+text-align: center;
+`
