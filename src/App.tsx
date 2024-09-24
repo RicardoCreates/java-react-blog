5dddb2e0e5d1b209ff81b0a2334b6063c09f17fe
Ricardo Lemser <ricardo.lemser@gmail.com>
2024-09-24 12:40:31 +0200

    <BrowserRouter>             <App/>         </BrowserRouter>

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import GlobalStyles from "./Globalstyles.ts";
 import Nav from "./components/Nav.tsx";
 import Footer from "./components/Footer.tsx";
 // import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import {HashRouter as Router, Route, Routes} from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 import About from "./About.tsx";
 import Startpage from "./components/Startpage.tsx";
 import ArticleCard from "./components/ArticleCard.tsx";
@@ -17,21 +17,20 @@ function App() {
 
     return (
         <>
-            <Router>
-                <GlobalStyles/>
-                <Header/>
-                <Nav/>
-                <main>
-                    <Routes>
-                        <Route path={"/"} element={<Startpage/>}/>
-                        <Route path={"/article"} element={<ArticleCard/>}/>
-                        <Route path="/about" element={<About/>}/>
-                        <Route path={'/character/:id'} element={<Character />} />
-                    </Routes>
-                </main>
-                <Nav2/>
-                <Footer/>
-            </Router>
+            <GlobalStyles/>
+            <Header/>
+            <Nav/>
+            <main>
+                <Routes>
+                    <Route path={"/"} element={<Startpage/>}/>
+                    <Route path={"/article"} element={<ArticleCard/>}/>
+                    <Route path="/about" element={<About/>}/>
+                    <Route path={'/character/:id'} element={<Character/>}/>
+                </Routes>
+            </main>
+            <Nav2/>
+            <Footer/>
+
         </>
     )
 }
